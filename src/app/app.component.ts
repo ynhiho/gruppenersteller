76509345484977b04fbed195914e935abadfc971
ynhiho <yho@pagemachine.de>
2023-04-20 03:15:16 +0200

    basic project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'team-generator-app';
+  newMemberNameInput = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  onInput(member: string){
+    this.newMemberNameInput = member;
+  }
+
+  addMember(){
+
+    if(!this.newMemberNameInput){
+      this.errorMessage = "Name can't be empty";
+      return
+    }
+
+    this.errorMessage = '';
+    this.members.push(this.newMemberNameInput);
+    this.newMemberNameInput = "";
+    console.log(this.members);
+  }
+
 }
